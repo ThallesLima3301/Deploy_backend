e668a2c148c80fd4c6c904ee0e6924cc2f5a5bde
Thalles Lima <thalles.lima3301@gmail.com>
2025-06-07 20:10:03 -0300

    corrigindo porta e endpoint

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const feedbackRoutes = require('./routes/feedback');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 
 app.use(cors());
 app.use(express.json());
